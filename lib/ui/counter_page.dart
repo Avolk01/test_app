3ec109e57e60f4ca3e9923e352a4cd4c5dbabe40
AVolk <volkodavartem01@mail.ru>
2021-08-26 15:47:24 +0300

    добавил loading screen

--- lib/ui/counter_page.dart
+++ lib/ui/counter_page.dart
@@ -8,6 +8,8 @@ import 'package:register_page/models/firebase_controller.dart';
 import 'package:register_page/statistic_bloc/statistic_bloc.dart';
 import 'package:register_page/ui/statistic_page.dart';
 import 'package:register_page/ui/widgets/img_with_button.dart';
+import 'package:register_page/ui/widgets/loading_help_screen.dart';
+import 'package:register_page/ui/widgets/main_splash_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 import 'charts.dart';
@@ -27,7 +29,8 @@ class _CounterPageState extends State<CounterPage> {
   int _giveUpCounter = 0;
   int _chetkoCounter = 0;
   String _uid;
-  CollectionReference users = FirebaseFirestore.instance.collection(FirebaseStrings.mainCollection);
+  CollectionReference users =
+      FirebaseFirestore.instance.collection(FirebaseStrings.mainCollection);
   bool start = true;
 
   void counterPageButton() {
@@ -43,7 +46,8 @@ class _CounterPageState extends State<CounterPage> {
           child: BlocProvider(
             create: (context) => StatisticBloc(
                 FirebaseController(
-                    mainCollection: FirebaseStrings.mainCollection, optionalCollection: FirebaseStrings.optionalCollection),
+                    mainCollection: FirebaseStrings.mainCollection,
+                    optionalCollection: FirebaseStrings.optionalCollection),
                 _uid),
             child: StatisticPage(),
           ),
@@ -55,11 +59,18 @@ class _CounterPageState extends State<CounterPage> {
   void exitButton() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove(FieldsStrings.uid);
-    Navigator.popUntil(context, (context) {
-      return true;
-    });
+    Navigator.popUntil(
+      context,
+      (context) {
+        return true;
+      },
+    );
     Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => HomePage()));
+      context,
+      MaterialPageRoute(
+        builder: (context) => HomePage(),
+      ),
+    );
   }
 
   List<int> _clicks = [];
@@ -67,114 +78,119 @@ class _CounterPageState extends State<CounterPage> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<CounterBloc, CounterState>(
-      listener: (context, state) {
-        if (state is GraphData) {
-          _clicks = state.values;
-          _dates = state.dates;
-        }
-        _blinCounter = state.blin;
-        _suicideCounter = state.suicide;
-        _giveUpCounter = state.giveUp;
-        _chetkoCounter = state.chetko;
-      },
-      child: StreamBuilder<QuerySnapshot>(
-        stream: users.snapshots(),
-        builder: (context, snapshot) {
-          return BlocBuilder(
-            bloc: context.watch<CounterBloc>(),
-            builder: (context, state) {
-              return Scaffold(
-                drawer: Drawer(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          FlatButton(
-                              onPressed: counterPageButton,
-                              child: Text(DrawerButtonStrings.back)),
-                          FlatButton(
-                              onPressed: statisticPageButton,
-                              child: Text(DrawerButtonStrings.stat)),
-                        ],
-                      ),
-                      FlatButton(
-                          onPressed: exitButton,
-                          child: Text(DrawerButtonStrings.exit)),
-                    ],
-                  ),
+    return Scaffold(
+      drawer: Drawer(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FlatButton(
+                  onPressed: counterPageButton,
+                  child: Text(DrawerButtonStrings.back),
                 ),
-                appBar: AppBar(
-                  title: Text(TitleStrings.counterPageTitle),
+                FlatButton(
+                  onPressed: statisticPageButton,
+                  child: Text(DrawerButtonStrings.stat),
                 ),
-                body: Column(
+              ],
+            ),
+            FlatButton(
+              onPressed: exitButton,
+              child: Text(DrawerButtonStrings.exit),
+            ),
+          ],
+        ),
+      ),
+      appBar: AppBar(
+        title: Text(TitleStrings.counterPageTitle),
+      ),
+      body: BlocConsumer<CounterBloc, CounterState>(
+        listener: (context, state) {
+          if (state is LoadingState) {}
+
+          if (state is GraphData) {
+            _clicks = state.values;
+            _dates = state.dates;
+          }
+        },
+        bloc: context.watch<CounterBloc>(),
+        builder: (context, state) {
+          if (state is IncCounterState || state is GraphData) {
+            return Column(
+              children: [
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SizedBox(
-                      height: 30,
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Column(
-                          children: <Widget>[
-                            ImageButton(
-                                ImagePath.blin,
-                                200,
-                                100,
-                                () => context
-                                    .read<CounterBloc>()
-                                    .add(IncCount(1))),
-                            Text('количество нажатий: $_blinCounter'),
-                            SizedBox(
-                              height: 33,
-                            ),
-                            ImageButton(
-                                ImagePath.suicide,
-                                200,
-                                40,
-                                () => context
-                                    .read<CounterBloc>()
-                                    .add(IncCount(2))),
-                            Text('количество нажатий: $_suicideCounter'),
-                            SizedBox(
-                              height: 33,
-                            ),
-                            ImageButton(
-                                ImagePath.giveUp,
-                                200,
-                                50,
-                                () => context
-                                    .read<CounterBloc>()
-                                    .add(IncCount(3))),
-                            Text('количество нажатий: $_giveUpCounter'),
-                          ],
+                    Column(
+                      children: <Widget>[
+                        ImageButton(
+                          ImagePath.blin,
+                          200,
+                          100,
+                          () => context.read<CounterBloc>().add(
+                                IncCount(1),
+                              ),
                         ),
+                        Text('количество нажатий: ${state.blin}'),
                         SizedBox(
-                          width: 20,
+                          height: 33,
                         ),
-                        Column(
-                          children: [
-                            ImageButton(ImagePath.chetko, 170, 295, () {
-                              context.read<CounterBloc>().add(IncCount(4));
-                            }),
-                            Text('количество нажатий: $_chetkoCounter'),
-                          ],
+                        ImageButton(
+                          ImagePath.suicide,
+                          200,
+                          40,
+                          () => context.read<CounterBloc>().add(
+                                IncCount(2),
+                              ),
                         ),
+                        Text('количество нажатий: ${state.suicide}'),
+                        SizedBox(
+                          height: 33,
+                        ),
+                        ImageButton(
+                          ImagePath.giveUp,
+                          200,
+                          50,
+                          () => context.read<CounterBloc>().add(
+                                IncCount(3),
+                              ),
+                        ),
+                        Text('количество нажатий: ${state.giveUp}'),
                       ],
                     ),
                     SizedBox(
-                      height: 150,
-                      width: 300,
-                      child: SimpleTimeSeriesChart(_clicks, _dates, false),
-                    )
+                      width: 20,
+                    ),
+                    Column(
+                      children: [
+                        ImageButton(
+                          ImagePath.chetko,
+                          170,
+                          295,
+                          () => context.read<CounterBloc>().add(
+                                IncCount(4),
+                              ),
+                        ),
+                        Text('количество нажатий: ${state.chetko}'),
+                      ],
+                    ),
                   ],
                 ),
-              );
-            },
-          );
+                SizedBox(
+                  height: 150,
+                  width: 300,
+                  child: SimpleTimeSeriesChart(_clicks, _dates, false),
+                )
+              ],
+            );
+          }
+            return MainSplashScreen();
         },
       ),
     );
